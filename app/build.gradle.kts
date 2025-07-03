eb5609b00046eb1a6956acdbcc55518d13bb1afe
akshatgour-dev <akshatgour19@gmail.com>
2025-07-03 18:47:52 +0530

    Enabled core library desugaring for java.time API support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        coreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
         jvmTarget = "11"
@@ -59,4 +60,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
